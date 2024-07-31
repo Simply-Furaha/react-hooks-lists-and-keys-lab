c658be9f32e34cb7117cef9fb5017510515a7f28
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-31 11:20:24 +0300

    Solving hooks, Lists & Keys

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {projects.map((projects)=> (
+      <ProjectItem 
+      key={projects.id} 
+      name={projects.name} 
+      about={projects.about} 
+      technologies={projects.technologies}
+      />))}
+      </div>
     </div>
   );
 }
